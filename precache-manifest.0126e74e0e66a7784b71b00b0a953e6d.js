d8e11a253d43057e93001ebaff19669addd040ce
mottuanand <mottu.ka@inapp.com>
2023-06-20 19:55:45 +0530

    Updates

---
+++ precache-manifest.0126e74e0e66a7784b71b00b0a953e6d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5f50e2a4d16b9e18b3ed32c52a2f99",
+    "url": "/grapejs-react-sample/index.html"
+  },
+  {
+    "revision": "28763f73c8bcf9b65e7d",
+    "url": "/grapejs-react-sample/static/css/main.0188ddd0.chunk.css"
+  },
+  {
+    "revision": "396e273b665864a1f9d6",
+    "url": "/grapejs-react-sample/static/js/2.5cab2f2d.chunk.js"
+  },
+  {
+    "revision": "290bd141c9c7dd9dede7c43e2c6f62c4",
+    "url": "/grapejs-react-sample/static/js/2.5cab2f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28763f73c8bcf9b65e7d",
+    "url": "/grapejs-react-sample/static/js/main.9ee84caa.chunk.js"
+  },
+  {
+    "revision": "3fa6ba6daa903548b43d",
+    "url": "/grapejs-react-sample/static/js/runtime-main.9fe3bfa5.js"
+  },
+  {
+    "revision": "38b30fa0229248d8bd3a14c802046743",
+    "url": "/grapejs-react-sample/static/media/main-fonts.38b30fa0.ttf"
+  },
+  {
+    "revision": "638868fe8f33b0b45c29fc4e4fba99a9",
+    "url": "/grapejs-react-sample/static/media/main-fonts.638868fe.woff"
+  },
+  {
+    "revision": "eceff26a5f33cc81123f437a16a78dbe",
+    "url": "/grapejs-react-sample/static/media/main-fonts.eceff26a.svg"
+  },
+  {
+    "revision": "f7c56e52c2c44690a0e39d5a486d0c3b",
+    "url": "/grapejs-react-sample/static/media/main-fonts.f7c56e52.eot"
+  }
+]);
